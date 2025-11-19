726a9a0f6d2b1ad7dbca5b5617eae80d5cc4b40a
Cole Cantu <colecantu904@gmail.com>
2025-11-18 21:59:12 -0500

    made comps folder

---
+++ src/app/generator/page.tsx
@@ -0,0 +1,17 @@
+export default function GeneratorPage() {
+  return (
+    <div className="min-h-screen flex flex-col mx-auto container justify-center px-4">
+      <div className="max-w-2xl">
+        <h2 className="text-4xl md:text-5xl font-bold">
+          Generate Your Holiday Experience
+        </h2>
+        <p className="text-xl md:text-2xl text-gray-600 dark:text-gray-400 mt-4">
+          To organize the participants for your secret santa event, please
+          provide each of the participants names and emails below, you can then
+          choose to personalize the message or send out ai generated
+          instructions.
+        </p>
+      </div>
+    </div>
+  );
+}
